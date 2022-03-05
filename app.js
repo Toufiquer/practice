f861d65ed15829a8df067ab3b33fcf8ee28b0968
Toufiquer Tahman <toufiquerabc@gmail.com>
2022-03-06 05:29:30 +0600

    1.0.0

--- app.js
+++ app.js
@@ -7,6 +7,47 @@ let warning = (data, isShow) => {
         dQS('#display-none').style.display = 'none'
     }
 }
+// *****************************************************
+let getCart = () => {
+    let cart = localStorage.getItem('cart')
+    let cartObj
+    if (cart) {
+        cartObj = JSON.parse(cart)
+    } else {
+        cartObj = {}
+    }
+    return cartObj
+}
+let makeData = (name, price) => {
+    let cart = getCart()
+    let newCart = JSON.stringify(cart)
+    if (newCart === '{}') {
+        console.log('empty')
+    } else {
+        console.log('Not empty')
+    }
+    console.log(cart)
+}
+let saveItem = (name, price) => {
+    makeData(name, price)
+    console.log(name, price)
+}
+// *****************************************************
+let displayItem = (name, price) => {
+    let div = document.createElement('div')
+    div.classList.add = 'container'
+    div.innerHTML = `
+        <div class="row">
+            <div class="col-6">
+                <h2>Product Name: <span id="name">${name}</span></h2>
+            </div>
+            <div class="col-6">
+                <h2>Price $$: <span id="price">${price}</span></h2>
+            </div>
+        </div>
+    `
+    dQS('#main').appendChild(div)
+}
 dQS('#price-input').addEventListener('keyup', () => {
     let priceInput = dQS('#price-input').value
     if (priceInput == '') {
@@ -20,25 +61,15 @@ dQS('#price-input').addEventListener('keyup', () => {
     }
 })
 dQS('#add').addEventListener('click', () => {
-    console.log('working')
+    // console.log('working')
     let product = dQS('#product').value
     let priceInput = dQS('#price-input').value
     if (((product === '') && (priceInput === '')) || (isNaN(priceInput))) { return }
-    let div = document.createElement('div')
-    div.classList.add = 'container'
-    div.innerHTML = `
-        <div class="row">
-            <div class="col-6">
-                <h2>Product Name: <span id="name">${product}</span></h2>
-            </div>
-            <div class="col-6">
-                <h2>Price $$: <span id="price">${priceInput}</span></h2>
-            </div>
-        </div>
-    `
-    dQS('#main').appendChild(div)
-    console.log(product, priceInput)
+    saveItem(product, priceInput)
+    displayItem(product, priceInput)
+    // console.log(product, priceInput)
 })
 dQS('#clear').addEventListener('click', () => {
     dQS('#main').textContent = ''
+    localStorage.removeItem('product')
 })
